d32121e4ec1acc5740fc543695181cef7c9d6e73
furnapso <luke.boland95@gmail.com>
2024-05-16 21:54:12 +1000

    create tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import konstaConfig from "konsta/config"
+
+export default konstaConfig({
+    content: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+})
+
