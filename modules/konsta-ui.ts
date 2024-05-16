e2485b80b5c117d1d83ecaa5ad0477547acbd4f4
furnapso <luke.boland95@gmail.com>
2024-05-17 08:20:33 +1000

    add konsta ui module

---
+++ modules/konsta-ui.ts
@@ -0,0 +1,8 @@
+import {addComponentsDir, defineNuxtModule, resolvePath} from "@nuxt/kit";
+
+export default defineNuxtModule(async (inlineOptions, nuxt) => {
+    await addComponentsDir({
+        path: await resolvePath("@@/node_modules/konsta/vue/components"),
+        prefix: "k"
+    });
+});
