41fa92eea9ddf714b2dfffdd0ec2aac279b4a677
furnapso <luke.boland95@gmail.com>
2024-05-17 05:43:18 +1000

    add id util

---
+++ util/id.ts
@@ -0,0 +1,9 @@
+import type {BaseModel} from "~/model/model";
+import {v4 as uuid} from "uuid";
+
+
+export default function addIdIfMissing(value: BaseModel) {
+    if (!value.id) {
+        value.id = uuid();
+    }
+}
