5165c164afac3922568502c9512c2f48f284e898
furnapso <luke.boland95@gmail.com>
2024-05-17 08:45:06 +1000

    add clients page

---
+++ pages/clients/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import useNavbarTitle from "~/composables/navbarTitle";
+
+useHead({
+  title: "Clients"
+});
+
+useNavbarTitle().setNavbarTitle("Clients");
+</script>
+
+<template>
+  <k-block>Hello!</k-block>
+</template>
