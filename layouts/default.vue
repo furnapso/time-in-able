c40a3f04d7735ab77af09963d7d3347c34dc18e3
furnapso <luke.boland95@gmail.com>
2024-05-17 05:44:54 +1000

    add default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,21 +4,18 @@ import {kApp, Tabbar, TabbarLink} from "konsta/vue";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {faHouse} from "@fortawesome/free-solid-svg-icons";
 
-const theme = computed(() => Capacitor.getPlatform() === "android" ? "material" : "ios")
+const theme = computed(() => Capacitor.getPlatform() === "android" ? "material" : "ios");
 </script>
 
 <template>
   <k-app :theme="theme">
-    <div class="h-full w-full">
-      <slot/>
-      <Tabbar :icons="true" class="bottom-0 left-0 fixed">
-        <tabbar-link label="Home">
-          <template #icon>
-            <font-awesome-icon :icon="faHouse"/>
-          </template>
-        </tabbar-link>
-      </Tabbar>
-    </div>
     <slot/>
+    <Tabbar :icons="true" class="bottom-0 left-0 fixed">
+      <tabbar-link label="Home">
+        <template #icon>
+          <font-awesome-icon :icon="faHouse"/>
+        </template>
+      </tabbar-link>
+    </Tabbar>
   </k-app>
 </template>
