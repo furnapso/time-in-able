901ad7deb3ac8c5be65aaf4dad8e364d4898f4a3
furnapso <luke.boland95@gmail.com>
2024-05-17 10:09:22 +1000

    add client modal

--- pages/clients/index.vue
+++ pages/clients/index.vue
@@ -1,13 +1,26 @@
 <script lang="ts" setup>
 import useNavbarTitle from "~/composables/navbarTitle";
+import {useAppStore} from "~/store";
 
 useHead({
   title: "Clients"
 });
 
+const clients = computed(() => useAppStore().clients);
+
+const clientModalOpen = ref(false);
+
 useNavbarTitle().setNavbarTitle("Clients");
 </script>
 
 <template>
-  <k-block>Hello!</k-block>
+  <k-toolbar :outline="false" inner-class="!justify-end">
+    <k-link toolbar @click="clientModalOpen = true">Add</k-link>
+  </k-toolbar>
+  <k-block strong>
+    <p v-if="clients.length === 0">
+      You don't have any clients. Click the Add button to add a new client.
+    </p>
+  </k-block>
+  <add-edit-client-modal :open="clientModalOpen" @close="clientModalOpen = false"/>
 </template>
