45e043abcacbb3d5ddf4cbf4431d0a4dcb6d8707
furnapso <luke.boland95@gmail.com>
2024-05-17 10:21:44 +1000

    support force validate

--- composables/nameValidator.ts
+++ composables/nameValidator.ts
@@ -1,11 +1,9 @@
 import type {Ref} from "vue";
 import isEmpty from "validator/es/lib/isEmpty";
 import isAlpha from "validator/es/lib/isAlpha";
+import type {NameValue} from "~/components/ClientModal.vue";
 
-export default function useNameValidator(nameRef: Ref<{
-    name?: string | null,
-    changed: boolean
-}>, forceValidate: boolean) {
+export default function useNameValidator(nameRef: Ref<NameValue>, forceValidate: boolean) {
     function validateName() {
         if (!nameRef.value.changed && !forceValidate) return null;
         const name = nameRef.value.name;
